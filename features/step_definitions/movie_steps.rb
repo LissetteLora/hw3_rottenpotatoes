7c958c840965c96962533d4882ac71d6b300d707
LissetteLora <lakatira00@gmail.com>
2013-07-22 00:14:24 -0300

    Update movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,8 +2,9 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+        Movie.create(movie)
+    end
+    assert movies_table.hashes.size == Movie.all.count
   end
   flunk "Unimplemented"
 end
@@ -12,8 +13,8 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
+    titles = page.all("table#movies tbody tr td[1]").map {|t| t.text}
+    assert titles.index(e1) < titles.index(e2)
   flunk "Unimplemented"
 end
 
